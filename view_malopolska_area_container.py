7585f69eadcb4504ead692128dca5e9a5fabb51d
LMalko86 <codewars@onet.pl>
2017-11-21 00:47:56 +0100

    Refactored name one column collection

--- view_malopolska_area_container.py
+++ view_malopolska_area_container.py
@@ -1,6 +1,6 @@
 class ViewMalopolskaAreaContainer():
 
-    def pprint_one_column(self, collection, column_name="MAŁOPOLSKIE"):
+    def pprint_one_column_collection(self, collection, column_name="MAŁOPOLSKIE"):
         top_and_bottom_table = "@------------------------------------@"
         print(top_and_bottom_table)
         print("|       " + str(column_name) + "                  |")
