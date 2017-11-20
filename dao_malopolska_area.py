64bddf9ecfbf10e85720bae1da98d962ac81d71e
LMalko86 <codewars@onet.pl>
2017-11-20 16:01:59 +0100

    entered impotr data to init

--- dao_malopolska_area.py
+++ dao_malopolska_area.py
@@ -5,10 +5,11 @@ class DAOMalopolskaArea():
 
     def __init__(self, filename="malopolska_area.csv"):
         self.filename = filename
+        self.import_data()
 
     def import_data(self):
         with open(self.filename, "r", encoding="utf_8") as myfile:
             self.imported_data = myfile
 
-    def __extract_imported_data(self):
-        return [line for line in self.imported_data]
+    def extract_imported_data(self):
+        return self.imported_data
