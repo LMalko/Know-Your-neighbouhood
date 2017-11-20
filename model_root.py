1cfffdd0ceba18fca79b39f8a557073c08d63b12
LMalko86 <codewars@onet.pl>
2017-11-20 16:02:37 +0100

    Added set container call

--- model_root.py
+++ model_root.py
@@ -4,4 +4,11 @@ from dao_malopolska_area import DAOMalopolskaArea
 class ModelRoot():
 
     def __init__(self):
-        self.malopolska_area_container = dao_malopolska_area.__extract_imported_data()
+        self.imported_container_object = DAOMalopolskaArea().extract_imported_data()
+        self.set_container_object()
+
+    def set_container_object(self):
+        self.malopolska_area_container = self.imported_container_object
+
+    def get_malopolska_area_container(self):
+        return self.malopolska_area_container
