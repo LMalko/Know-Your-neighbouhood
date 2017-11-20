059b70c047fe3c0f98fc2a478ed48809bf315d6b
LMalko86 <codewars@onet.pl>
2017-11-20 13:51:02 +0100

    Defined viewroot class

---
+++ view_root.py
@@ -0,0 +1,29 @@
+import os
+
+
+class ViewRoot():
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def display_login_screen(self):
+        self.clear_screen()
+        _new_lines = '\n' * 4
+        user_name = input("Enter Your name: ")
+        return self.take_user_input(user_name)
+
+    def take_user_input(user_name):
+        return user_name
+
+    @staticmethod
+    def display_message(text):
+        print('\n\n' + text + '\n\n')
+
+    def display_menu_screen(self):
+        self.menu_options = ["(1) List statistics\n",
+                        "(2) Display 3 cities with longest names\n",
+                        "(3) Display county's name with the largest number of communities\n",
+                        "(4) Display locations, that belong to more than one category\n",
+                        "(5) Advanced search\n",
+                        "(0) Exit program\n"]
+        return self.menu_options
