92d6413301ef2b40f6d0b949160ed35dfd558430
LMalko86 <codewars@onet.pl>
2017-11-20 14:50:06 +0100

    Defined contr area conta class

---
+++ controller_malopolska_area_container.py
@@ -0,0 +1,29 @@
+from view_malopolska_area_container import ViewMalopolskaAreaContainer
+from model_malopolska_area_container import ModelMalopolskaAreaContainer
+from controller_malopolska_area import ControllerMalopolskaArea
+
+class ControllerMalopolskaAreaContainer():
+
+    def __init__(self):
+        self.associated_area = ContainerMalopolskaArea()
+
+    def get_area_name(self):
+        pass
+
+    def area_display(self):
+        pass
+
+    def list_by_unit(self):
+        pass
+
+    def get_top_areas_by_longest_name(self):
+        pass
+
+    def get_top_area_by_number_of_sub_units(self):
+        pass
+
+    def get_area_that_belongs_to_more_than_one_category(self):
+        pass
+
+    def get_search_for_expression_results(self):
+        pass
