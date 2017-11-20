44829d125e7ecfc7b7e1a94d7a1cd65b753a4ad2
LMalko86 <codewars@onet.pl>
2017-11-20 15:31:03 +0100

    Define view user class

--- view_user.py
+++ view_user.py
@@ -1,2 +1,17 @@
+import os
+
+
 class ViewUser():
-    pass
+
+    def get_user_input(self, text):
+        return input(text)
+
+    def display_collection(self, collection):
+        for item in collection:
+            print(item)
+
+    def display_message(self, text):
+        print(text)
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
