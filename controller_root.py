8635ec22c4767a243b41753557089471903c423d
LMalko86 <codewars@onet.pl>
2017-11-20 16:03:32 +0100

    Fixed filename -> class caall bug

--- controller_root.py
+++ controller_root.py
@@ -7,25 +7,24 @@ class ControllerRoot():
 
     def __init__(self):
         self.initialize_model()
-        self.view = view_root.ViewRoot()
+        self.view = ViewRoot()
 
     def initialize_model(self):
-        self.model = model_root.ModelRoot()
+        self.model = ModelRoot()
 
     def initialize_user_controller(self):
         self.user_controller = ControllerUser()
 
     def initialize_containers(self):
-        self.malopolska_area_container = model_root.import_malopolska_area_data()
+        self.malopolska_area_container = self.model.get_malopolska_area_container()
 
     def login(self):
-        self.login = view_root.display_login_screen()
+        self.login = ViewRoot().display_login_screen()
 
     def menu_screen(self):
-        self.menu = view_root.display_menu_screen()
+        self.menu = ViewRoot().display_menu_screen()
 
-    @staticmethod
-    def flow():
+    def flow(self):
         self.initialize_model()
         self.initialize_user_controller()
         self.initialize_containers()
