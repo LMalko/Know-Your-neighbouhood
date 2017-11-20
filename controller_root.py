61353648acc20cd2cd5b59199f6edc1ab69e26c8
LMalko86 <codewars@onet.pl>
2017-11-20 13:15:36 +0100

    Defined class ControllerRoot

---
+++ controller_root.py
@@ -0,0 +1,29 @@
+from model_root import *
+from view_root import *
+from controller_user import ControllerUser
+
+
+class ControllerRoot():
+
+    def __init__(self):
+        self.initialize_model()
+        self.view = view_root.ViewRoot()
+
+    def initialize_model(self):
+        self.model = model_root.ModelRoot()
+
+    def initialize_user_controller(self):
+        self.user_controller = ControllerUser()
+
+    def initialize_containers(self):
+        self.malopolska_area_container = model_root.import_malopolska_area_data()
+
+    def login(self):
+        self.login = view_root.display_login_screen
+
+    def menu_screen(self):
+        self.menu = view_root.display_menu_screen()
+
+
+
+    
