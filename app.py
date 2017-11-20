31d5aa04ca137dc75df28baf2642bc88ac19c7ff
LMalko86 <codewars@onet.pl>
2017-11-20 12:55:19 +0100

    Create module and class app

---
+++ app.py
@@ -0,0 +1,13 @@
+from controller_root import ControllerRoot
+
+
+class App():
+
+    def __init__(self):
+
+        self.root = ControllerRoot
+
+
+if __name__ == "__main__":
+    initiate_program = App()
+    initiate_program.root.start()
