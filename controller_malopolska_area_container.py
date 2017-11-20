e111298e2f94901bcecadcaa9972dc278e46c41f
LMalko86 <codewars@onet.pl>
2017-11-20 21:48:14 +0100

    Defined get cool by unit

--- controller_malopolska_area_container.py
+++ controller_malopolska_area_container.py
@@ -1,11 +1,16 @@
 from view_malopolska_area_container import ViewMalopolskaAreaContainer
 from model_malopolska_area_container import ModelMalopolskaAreaContainer
+from dao_malopolska_area import DAOMalopolskaArea
 from controller_malopolska_area import ControllerMalopolskaArea
+from model_malopolska_area import ModelMalopolskaArea
+
 
 class ControllerMalopolskaAreaContainer():
 
     def __init__(self):
-        self.associated_area = ContainerMalopolskaArea()
+        self.associated_areas = DAOMalopolskaArea().extract_imported_data()
+        self.associated_areas_objects = ControllerMalopolskaArea(self.associated_areas).get_areas_objects()
+        self.ModelMalopolskaAreaContainer = ModelMalopolskaAreaContainer(self.associated_areas_objects)
 
     def get_area_name(self):
         pass
@@ -13,8 +18,8 @@ class ControllerMalopolskaAreaContainer():
     def area_display(self):
         pass
 
-    def list_by_unit(self):
-        pass
+    def get_collection_by_unit(self):
+        return self.ModelMalopolskaAreaContainer.sort_units_count_by_size()
 
     def get_top_areas_by_longest_name(self):
         pass
@@ -27,3 +32,9 @@ class ControllerMalopolskaAreaContainer():
 
     def get_search_for_expression_results(self):
         pass
+
+a = ControllerMalopolskaAreaContainer()
+'''
+for i in a.associated_areas_objects:
+    print(i.set_area())'''
+print(a.get_collection_by_unit())
