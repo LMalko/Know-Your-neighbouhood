e840fdec03dc446bb2183319d34f76b5663f9c7d
LMalko86 <codewars@onet.pl>
2017-11-20 19:14:50 +0100

    Defined all methods

--- controller_malopolska_area.py
+++ controller_malopolska_area.py
@@ -3,11 +3,15 @@ from model_malopolska_area import ModelMalopolskaArea
 
 class ControllerMalopolskaArea():
 
-    def __init__(self):
-        self.malopolska_area = None
+    def __init__(self, data):
+        self.malopolska_areas = data
+        self.set_areas_objects()
 
-    def get_area_name(self, ModelMalopolskaArea):
-        self.malopolska_area = ModelMalopolskaArea.display_area()
+    def set_areas_objects(self):
+        disregard_categories_list = 1
+        # x[4], x[5], x[1], x[2], x[3] stand for name, unit type, county, municipality and rgmi respectively.
+        self.malopolska_areas_objects = [ModelMalopolskaArea(x[4], x[5], x[1], x[2], x[3]) 
+                                         for x in self.malopolska_areas[disregard_categories_list:]]
 
-    def area_display(self, ModelMalopolskaArea):
-        return self.malopolska_area
+    def get_areas_objects(self):
+        return self.malopolska_areas_objects
