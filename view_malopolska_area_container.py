45f2d420c96b36cbdce326e728cecee22b565d0c
LMalko86 <codewars@onet.pl>
2017-11-21 00:37:07 +0100

    Define pprint one column

--- view_malopolska_area_container.py
+++ view_malopolska_area_container.py
@@ -1,4 +1,10 @@
 class ViewMalopolskaAreaContainer():
 
-    def pprint_collection(self, collection):
-        pass
+    def pprint_one_column(self, collection, column_name="MAŁOPOLSKIE"):
+        top_and_bottom_table = "@------------------------------------@"
+        print(top_and_bottom_table)
+        print("|       " + str(column_name) + "                  |")
+        for i in collection:
+            print("|----+-------------------------------|")
+            print("|{}|{}|".format('{:>4}'.format(i[1]), '{:>31}'.format(i[0])))
+        return top_and_bottom_table
