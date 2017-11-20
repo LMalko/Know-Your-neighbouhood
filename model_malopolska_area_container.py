fdf90fb4b200436a86463143df895e121ac66ea1
LMalko86 <codewars@onet.pl>
2017-11-20 23:33:11 +0100

    Defined sorting methods

--- model_malopolska_area_container.py
+++ model_malopolska_area_container.py
@@ -25,11 +25,26 @@ class ModelMalopolskaAreaContainer():
         self.areas_wanted = [x for x in self.area_container if x.unit == area_type]
         return sorted(self.areas_wanted, key=lambda x: len(x.name), reverse=True)[0:top]
 
-    def set_top_areas_by_number_of_sub_units(self, top=1, area_type="county"):
-        pass
+    def sort_counties_by_number_of_municipalities(self):
+        return Counter([x.county for x in self.area_container])
 
-    def set_area_that_belongs_to_more_than_one_category(self, appearences=2):
-        pass
+    def set_top_county_by_number_of_municipalities(self):
+        sorted_counties_by_subunits = self.sort_counties_by_number_of_municipalities()
+        largest_county = sorted(sorted_counties_by_subunits.values(), key=lambda value: value, reverse=True)[0]
+        for key, value in sorted_counties_by_subunits.items():
+            if value == largest_county:
+                return key
+
+    def sort_cities_by_number_of_appearences(self):
+        return Counter([x.name for x in self.area_container])
+
+    def set_areas_that_belong_to_more_than_one_category(self, appearences=2):
+        sort_cities_by_number_of_appearences = self.sort_cities_by_number_of_appearences()
+        result = []
+        for key, value in sort_cities_by_number_of_appearences.items():
+            if value >= appearences:
+                result.append("{} belongs to {} categories.".format(key, value))
+        return result
 
     def search_for_expression(self, expression):
-        pass
+        return [x for x in self.area_container if expression in x.name.lower()]
