f01d53e098e891c995b1b0325d2ef91169d48f10
LMalko86 <codewars@onet.pl>
2017-11-20 17:46:32 +0100

    Fixed self.imported_data

--- dao_malopolska_area.py
+++ dao_malopolska_area.py
@@ -9,7 +9,10 @@ class DAOMalopolskaArea():
 
     def import_data(self):
         with open(self.filename, "r", encoding="utf_8") as myfile:
-            self.imported_data = myfile
+            self.imported_data = [line for line in myfile]
 
     def extract_imported_data(self):
         return self.imported_data
+
+a = DAOMalopolskaArea()
+print(a.extract_imported_data())
