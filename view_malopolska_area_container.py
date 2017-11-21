64bf08e0aaa7aa852b91cf876349ccc8299d0d4a
LMalko86 <codewars@onet.pl>
2017-11-21 14:00:20 +0100

    Adjust pprint to fit more choices

--- view_malopolska_area_container.py
+++ view_malopolska_area_container.py
@@ -1,10 +1,10 @@
 class ViewMalopolskaAreaContainer():
 
-    def pprint_one_column_collection(self, collection, column_name="MAŁOPOLSKIE"):
+    def pprint_one_column_collection(self, collection, table_header):
         top_and_bottom_table = "@------------------------------------@"
         print(top_and_bottom_table)
-        print("|       " + str(column_name) + "                  |")
+        print("|" + str(table_header) + "                         |")
         for i in collection:
             print("|----+-------------------------------|")
             print("|{}|{}|".format('{:>4}'.format(i[1]), '{:>31}'.format(i[0])))
-        return top_and_bottom_table
+        print(top_and_bottom_table)
