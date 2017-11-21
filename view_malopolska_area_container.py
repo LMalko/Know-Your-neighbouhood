052038d7d3345c9e0eb1edf0d2ce9c004794e632
LMalko86 <codewars@onet.pl>
2017-11-21 14:38:26 +0100

    Defined pprint lines method

--- view_malopolska_area_container.py
+++ view_malopolska_area_container.py
@@ -8,3 +8,8 @@ class ViewMalopolskaAreaContainer():
             print("|----+-------------------------------|")
             print("|{}|{}|".format('{:>4}'.format(i[1]), '{:>31}'.format(i[0])))
         print(top_and_bottom_table)
+
+    def pprint_lines(self, collection, table_header):
+        print("\n" + table_header + "\n")
+        for i in collection:
+            print("{}. {}".format(collection.index(i), i))
