a76dfebda93b3e58c8816a39c40057aa4e61c6c5
LMalko86 <codewars@onet.pl>
2017-11-21 21:48:26 +0100

    addedset elements method, canceled unnecessary print

--- view_malopolska_area_container.py
+++ view_malopolska_area_container.py
@@ -1,15 +1,33 @@
 class ViewMalopolskaAreaContainer():
 
     def pprint_one_column_collection(self, collection, table_header):
-        top_and_bottom_table = "@------------------------------------@"
-        print(top_and_bottom_table)
-        print("|" + str(table_header) + "                         |")
+        print(self.set_elements_of_table_construction()[0],
+              self.set_elements_of_table_construction(table_header)[2],
+              sep='\n')
         for i in collection:
-            print("|----+-------------------------------|")
-            print("|{}|{}|".format('{:>4}'.format(i[1]), '{:>31}'.format(i[0])))
-        print(top_and_bottom_table)
+            print(self.set_elements_of_table_construction()[1],
+                  "|{}|{}|".format('{:>4}'.format(i[1]), '{:>31}'.format(i[0])),
+                  sep='\n')
+        print(self.set_elements_of_table_construction()[0])
+
+    def pprint_two_columns_collection(self, collection, table_header):
+        print(self.set_elements_of_table_construction()[4],
+              self.set_elements_of_table_construction(table_header)[3],
+              self.set_elements_of_table_construction()[5],
+              sep='\n')
+        for i in collection:
+            print("|{}|{}|".format('{:>21}'.format(i[0]), '{:>25}'.format(i[1])))
+        print(self.set_elements_of_table_construction()[4])
 
     def pprint_lines(self, collection, table_header):
         print("\n" + table_header + "\n")
         for i in collection:
-            print("{}. {}".format(collection.index(i), i))
+            print("{}. {}".format(collection.index(i) + 1, i))
+
+    def set_elements_of_table_construction(self, header=None):
+        return ["@------------------------------------@",
+                "|----+-------------------------------|",
+                "|" + str(header) + "                         |",
+                "| LOCATION            | TYPE                    |",
+                "@-----------------------------------------------@",
+                "|---------------------+-------------------------|"]
