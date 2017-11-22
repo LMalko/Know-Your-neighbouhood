d73e76ed92c2aa03e35f022e93b31e2bf116db8f
LMalko86 <codewars@onet.pl>
2017-11-22 02:33:11 +0100

    Deleted unnecessary attribute

--- dao_malopolska_area.py
+++ dao_malopolska_area.py
@@ -5,11 +5,11 @@ class DAOMalopolskaArea():
 
     def __init__(self, filename="malopolska_area.csv"):
         self.filename = filename
-        self.import_data()
+        self.imported_data = self.import_data()
 
     def import_data(self):
         with open(self.filename, "r", encoding="utf_8") as myfile:
-            self.imported_data = [line.split("\n")[0].split(",") for line in myfile]
+            return [line.split("\n")[0].split(",") for line in myfile]
 
     def extract_imported_data(self):
         return self.imported_data
