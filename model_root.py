d7ef0c5974172061141bcdbfde1b1dde2194275f
LMalko86 <codewars@onet.pl>
2017-11-22 02:32:27 +0100

    Deleted unnecessary attribute

--- model_root.py
+++ model_root.py
@@ -4,11 +4,7 @@ from dao_malopolska_area import DAOMalopolskaArea
 class ModelRoot():
 
     def __init__(self):
-        self.imported_container_object = DAOMalopolskaArea().extract_imported_data()
-        self.set_container_object()
-
-    def set_container_object(self):
-        self.malopolska_area_container = self.imported_container_object
+        self.malopolska_area_container = DAOMalopolskaArea().extract_imported_data()
 
     def get_malopolska_area_container(self):
         return self.malopolska_area_container
